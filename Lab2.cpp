57a796c906dc98123f2436e95631ee181f2c66b9
rodrigez6 <rodrigez6@ukr.net>
2017-04-11 17:50:25 +0300

    Added main function

--- Lab2.cpp
+++ Lab2.cpp
@@ -1,30 +1,47 @@
-// by Mykola Danyliuk and Kostya Hudenko 
+// Lab2.cpp: определяет точку входа для консольного приложения.
+//
 
+#include "stdafx.h"
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <iomanip>
-#include <hash_map>
+#include <unordered_map>
 using namespace std;
+unordered_map<string, string> dictionary;
+void read();
+void start();
 
-//hash_map<string, string> dictionary;
-void reading_file();
-
-int main(int argc, const char * argv[]) {
-	reading_file();
-	return 0;
+int main()
+{
+	read();
+	start();
+    return 0;
 }
 
-void reading_file() {
-	hash_map<string, string> dictionary;
-	string meaning,word;
-	ifstream fin("https://github.com/ProgramEngineeringKPI/Introduction-To-Programming/raw/master/labs_spring_2017/dict_processed.txt");
-	while (!fin.eof()) {
+void read() {
+	string meaning, word;
+	//ifstream fin("https://github.com/ProgramEngineeringKPI/Introduction-To-Programming/raw/master/labs_spring_2017/dict_processed.txt");
+	ifstream fin("in.txt");
+	int counter=0;
+	getline(fin, word, '\n');
+	cout << word << endl;
+	while (fin.eof()) {
 		getline(fin, word, ';');
 		getline(fin, meaning, '\n');
-		dictionary.insert(word, meaning);
+		dictionary[word] = meaning;
+		counter++;
+		if(counter%100000==0) cout<< counter/100000 << " " << meaning << endl;
 	}
 	fin.close();
-	cout << dictionary.size << endl;
 }
 
+void start() {
+	cout << "Dictionary" << endl;
+	string word;
+	while (true) {
+		if (word == "end") break;
+		cin >> word ;
+		cout << dictionary[word] << endl;
+	}
+}
